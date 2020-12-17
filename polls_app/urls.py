1e68b73af3ed7ee9fa6da44b5bad5c657a1de0cc
shizuya <bubbler018@gmail.com>
2020-12-17 23:17:24 +0900

    エラーが出ます

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 app_name = 'polls_app'
 urlpatterns = [
-    path("", views.index,name='index'),
-    # ex: /polls_test/5/
-    path('<int:question_id>/',views.detail,name='detail'),
-    # ex: /polls_test/5/results/
-    path('<int:question_id>/results/',views.results,name='results'),
-    # ex: /polls_test/5/vote/
-    path('<int:question_id>/vote/',views.vote,name='vote'),
+    #path("", views.index, name='index'),
+    #path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/results/', views.results, name='results'),
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
